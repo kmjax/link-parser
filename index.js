e95663c8999f682a9f9eea9185ad2016bd477b4b
Kelsey McClanahan <jaxcode@kmcode.com>
2020-11-27 16:04:18 -0500

    Added basic core web page files to the project

---
+++ index.js
@@ -0,0 +1,13 @@
+// ===============================================
+// Code to parse a URL into it's base query link
+// and the individual parameters
+//
+// Jaxcode Day 11 Homework Assignment
+//
+// Author: Kelsey McClanahan
+// Date:   11/27/2020
+// ===============================================
+
+alert(
+  "Hello World\nThis lets us know the JavaScript\nsource is linked with this page.\n\nThis form is non-functional at the moment."
+);
